f6e0c0b229d802d848b57b53d43e794b4d96ca1b
Ian Diaz <idiaz@itba.edu.ar>
2017-05-30 21:43:24 -0300

    Created first draft of startAuto1() and associated functions

--- Intelligence.c
+++ Intelligence.c
@@ -97,8 +97,8 @@ typedef struct
 	double y;
 }coordinate_s;
 
-sensorInfo_s allInfo[SENSORS_AMMOUNT];	//tiene que estar creado en algun lugar. NO GLOBAL
-coordinate_s allCoordinates[SENSORS_AMMOUNT];
+static sensorInfo_s allInfo[SENSORS_AMMOUNT];	//tiene que estar creado en algun lugar. NO GLOBAL
+static coordinate_s allCoordinates[SENSORS_AMMOUNT];
 
 //proptotipos
 void initSensorsData(sensorInfo_s* allInfo);
@@ -256,86 +256,96 @@ void I_Update(int16_t _intelligenceType)
 /////////////////////////////////////////////////////////////////////////
 static void startAuto1(voids)
 {
-	int left;
-	left = checkLeft();
-	int leftAndUp = checkLefAndUp();
-	double cosHipo;
-	int up = checkUp();
-
-	//Necesito las funciones de malee
-
-	if (up > MIN_DISTANCE)
+	int i;
+	checkPointsOnXAxis();
+	if(PointsOnXAxis[0] !=(SENSORS_AMMOUNT+1))
 	{
-		if (((cosHipo = leftAndUp * cos(ANGLE)) > (left - 1)) && (cosHipo) < (left + 1) && up > MIN_DISTANCE)
-			moveFoward();
-		else if (cosHipo < (left - 1))
-			turn((int)(cosHipo - left));
-		else if (cosHipo >(left + 1))
-			trun((int)(cosHipo - left));
-	}
-	else
-	{
-		if (((cosHipo = leftAndUp * cos(ANGLE)) > (left - 1)) && (cosHipo) < (left + 1) && up > MIN_DISTANCE)
-			turn(IAN_RIGHT);
-		else if (cosHipo < (left - 1))
-			turn((int)(cosHipo - left));
+		if(PointsOnXAxis[1] !=(SENSORS_AMMOUNT+1))
+		{
+			if(!somethingAhead())
+			{
+				double inclination = checkInclination(allCoordinates[PointsOnXAxis[0]],allCoordinates[PointsOnXAxis[1]]);
+				actuateOverInclination(inclination);
+			}
+			else
+				trunRight();
+		}
 		else
-			turn(IAN_RIGHT);
+		{
+			if(!somethingAhead())
+				moveFoward();
+			else
+				turnRight();
+		}
 	}
 	return;
 }
 
+//////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////
-static void turn(int definer)
+
+static int PointsOnXAxis[2];
+#define I_MIN_DISTANCE_BETWEEN_POINTS	0.5
+/*Funcion que no recibe nada y chequea el arreglo de allCoordinates buscando los numeros negativos de x
+y encontrando 2 puntos guarda el iterador de allCoordinates en PointsOnXAxis, ordenandolos de menor "y" de
+coordinate_s a mayor "y" de coordinate_s
+*/
+static void checkPointsOnXAxis(void)
 {
-	if (definer >= 0)
+	int i;
+	for (i =0; i<2;i++)			//Inicializo el arreglo de 2 ints en 0
+		PointsOnXAxis[i]=SENSORS_AMMOUNT+1;
+	for(i=0; (i<SENSORS_AMMOUNT) && (allCoordinates[i].x>0) ;i++);//Me muevo hasta encontrar un punto en x negativo o hasta recorrer todos los puntos obtenidos
+	if(allCoordinates[i].x<0)	//Chequeo si salio del for porque un sensor encontro un pto negativo
 	{
-		switch (definer) {
-		case 0:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 2);
-		case 1:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 10);
-			break;
-		case 2:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 12);
-			break;
-		case 3:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 15);
-			break;
-		case 4:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 17);
-			break;
-		case 5:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 20);
-			break;
-		default:
-			S_setActuatorMovUnproc(RIGHT_MOTOR, 22);
-			break;
-		}
-	}
-	else
-	{
-		switch (definer) {
-		case -1:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 10);
-			break;
-		case -2:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 12);
-			break;
-		case -3:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 15);
-			break;
-		case -4:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 17);
-			break;
-		case -5:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 20);
-			break;
-		default:
-			S_setActuatorMovUnproc(LEFT_MOTOR, 22);
-			break;
+		PointsOnXAxis[0] = i;		//Guardo el numero del pto en el erreglo PointsOnXAxis
+		bool exit = false;
+		for(i++; (i<SENSORS_AMMOUNT) && (exit == false) ;i++)
+		{
+			if(allCoordinates[i].x <0)
+			{
+				if((allCoordinates[i].y)>(allCoordinates[PointsOnXAxis[0]].y + I_MIN_DISTANCE_BETWEEN_POINTS))
+				{
+					allCoordinates[1] = i;
+					exit=true;
+				}
+				else if((allCoordinates[i].y)<(allCoordinates[PointsOnXAxis[0]].y - I_MIN_DISTANCE_BETWEEN_POINTS))
+				{
+					allCoordinates[1] = allCoordinates[0];
+					allCoordinates[0] = i;
+					exit = true;
+				}
+			}
 		}
 	}
 	return;
+}
+
+/*Funcion que devuelve la pendiente que se forma entre 2 puntos
+coordinate_s
+*/
+static double checkInclination(coordinate_s p0,coordinate_s p1)
+{
+	double deltaX = p0.x - p1.x;
+	double deltaY = p0.y - p1.y;
+	return (deltaX/deltaY);
+}
+
+
+/*Funcion que devuelve true si hay algo delante del robot
+o false si no hay nada delante del robot*/
+static bool somethingAhead(void)
+{
+	int i;
+	bool isThereSomethingAhead =  false;
+	for(i=0;(allCoordinates[i].y > 0) && (i < SENSORS_AMMOUNT);i++);
+	if(i == (SENSORS_AMMOUNT -1))
+		return false;
+	else
+		return true;
+}
+
+static void turnRight(void)
+{
+	//Hacer esta funcion!!!!!
 }
